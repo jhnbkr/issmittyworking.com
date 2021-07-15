206616bd1cad140d066f43182679147014b83da4
John Baker <john@theyoungastronauts.com>
2021-07-15 12:15:24 -0400

    do you think smitty is working?

--- project/settings.py
+++ project/settings.py
@@ -10,32 +10,17 @@ env_path = os.path.join(BASE_DIR, ".env")
 if os.path.exists(env_path):
     ENV.read_env(env_path)
 
+# region Application
+
 ALLOWED_HOSTS = ENV.list("ALLOWED_HOSTS", default=[])
 DEBUG = ENV.bool("DEBUG", default=False)
-SCHEDULE_PERIOD = ENV.int("SCHEDULE_PERIOD", default=4)
-SCHEDULE_PROVINCE = ENV.str("SCHEDULE_PROVINCE", default="AB")
-SCHEDULE_SHIFT_A_START = datetime.datetime.strptime(
-    ENV.str("SCHEDULE_SHIFT_A_START"), "%H:%M:%S"
-).time()
-SCHEDULE_SHIFT_A_END = datetime.datetime.strptime(
-    ENV.str("SCHEDULE_SHIFT_A_END"), "%H:%M:%S"
-).time()
-SCHEDULE_SHIFT_B_START = datetime.datetime.strptime(
-    ENV.str("SCHEDULE_SHIFT_B_START"), "%H:%M:%S"
-).time()
-SCHEDULE_SHIFT_B_END = datetime.datetime.strptime(
-    ENV.str("SCHEDULE_SHIFT_B_END"), "%H:%M:%S"
-).time()
-SCHEDULE_START_DATE = datetime.datetime.strptime(
-    ENV.str("SCHEDULE_START_DATE"), "%Y-%m-%d"
-).date()
 SECRET_KEY = ENV.str("SECRET_KEY")
 SECURE_SSL_REDIRECT = ENV.bool("SECURE_SSL_REDIRECT", default=True)
 TIME_ZONE = ENV.str("TIME_ZONE", default="UTC")
-USE_TZ = True
 
 WSGI_APPLICATION = "project.wsgi.application"
 ROOT_URLCONF = "project.urls"
+USE_TZ = True
 
 INSTALLED_APPS = [
     "rest_framework",
@@ -78,3 +63,27 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [],
     "DEFAULT_PERMISSION_CLASSES": [],
 }
+
+# endregion
+
+# region Schedule
+
+SCHEDULE_PERIOD = ENV.int("SCHEDULE_PERIOD", default=4)
+SCHEDULE_PROVINCE = ENV.str("SCHEDULE_PROVINCE", default="AB")
+SCHEDULE_SHIFT_A_START = datetime.datetime.strptime(
+    ENV.str("SCHEDULE_SHIFT_A_START"), "%H:%M:%S"
+).time()
+SCHEDULE_SHIFT_A_END = datetime.datetime.strptime(
+    ENV.str("SCHEDULE_SHIFT_A_END"), "%H:%M:%S"
+).time()
+SCHEDULE_SHIFT_B_START = datetime.datetime.strptime(
+    ENV.str("SCHEDULE_SHIFT_B_START"), "%H:%M:%S"
+).time()
+SCHEDULE_SHIFT_B_END = datetime.datetime.strptime(
+    ENV.str("SCHEDULE_SHIFT_B_END"), "%H:%M:%S"
+).time()
+SCHEDULE_START_DATE = datetime.datetime.strptime(
+    ENV.str("SCHEDULE_START_DATE"), "%Y-%m-%d"
+).date()
+
+# endregion
